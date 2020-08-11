a10b12252d2984619f7250756e7737cc29e356e3
krishna-incrypto <vpkrishnanand@gmail.com>
2020-08-11 22:39:40 +0530

    added google logo

--- lib/imageGen/imageGen.dart
+++ lib/imageGen/imageGen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
@@ -9,16 +10,20 @@ import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class ImgFromTempelate {
-  
   // Loads an Image from assets and conver it into a darts Image encoding
   static Future<ui.Image> loadUiImage(String imageAssetPath) async {
     final ByteData data = await rootBundle.load(imageAssetPath);
-    Future<ui.Image> img = decodeImageFromList(Uint8List.view(data.buffer));
-    return img;
+    final Completer<ui.Image> completer = Completer();
+    ui.decodeImageFromList(Uint8List.view(data.buffer), (ui.Image img) {
+      return completer.complete(img);
+    });
+    return completer.future;
   }
 
   // Prints a paragraph onto the canvas
-  static printParagraph({Canvas canvas,Offset offset,String text, bool fromMap,Map<String,String> map}) {
+  static ui.Paragraph createParagraph(
+      {String text, bool fromMap, Map<String, String> map}) {
+    print(map);
     final textStyle = ui.TextStyle(
       fontWeight: FontWeight.bold,
       color: Colors.white,
@@ -29,45 +34,114 @@ class ImgFromTempelate {
     );
     final paragraphBuilder = ui.ParagraphBuilder(paragraphStyle)
       ..pushStyle(textStyle);
-      if(fromMap){
-        map.forEach((key, value) {
+try {
+  if (fromMap) {
+      map.forEach((key, value) {
+        
+       
           paragraphBuilder.addText('\n$key : $value');
-        });
-      }else{
-        paragraphBuilder.addText(text);
-      }
+       
+      });
+    } else {
+      paragraphBuilder.addText(text);
+    }
+} catch (e) {
+   print('Cannot addText');
+}
     
 
-    final constraints = ui.ParagraphConstraints(width: 830);
     final paragraph = paragraphBuilder.build();
-    paragraph.layout(constraints);
+
+    print(paragraph.height);
     // final offset = Offset(75, 150);
-    
+    return paragraph;
+  }
+
+  static double printParagraph(
+      {Canvas canvas,
+      String text,
+      double widthConstraint,
+      Offset offset,
+      bool fromMap,
+      Map<String, String> map}) {
+    ui.Paragraph paragraph;
+    try {
+      print(map);
+      paragraph = createParagraph(fromMap: fromMap, text: text, map: map);
+    } catch (e) {
+      print('Error in printParagraph');
+    }
+
+    paragraph.layout(ui.ParagraphConstraints(width: widthConstraint));
     canvas.drawParagraph(paragraph, offset);
     return paragraph.height;
   }
 
+  static void printGpay(
+      {Canvas canvas, Offset offset, String no, ui.Image img}) async {
+    Paint paint = Paint();
+    paint.color = Colors.white;
+    paint.style = PaintingStyle.fill;
+
+    // final paragraph = createParagraph(fromMap: false,text: 'Pay : 7034320115');
+
+    // Draws a rounded rectangle
+    print("hello");
+    print(canvas);
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(offset & Size(700, 100), Radius.circular(20)),
+      paint,
+    );
+    // Offset(75, 150 + pH + 20)
+
+    // Draws the Gpay logo
+    paintImage(
+      canvas: canvas,
+      scale: 1,
+      fit: BoxFit.cover,
+      rect: Rect.fromCenter(
+        center: Offset(130, offset.dy + 50),
+        height: 80,
+        width: 80,
+      ),
+      image: img,
+    );
+
+    // Prints Number
+
+  }
+
 // The core funtion which puts all the drawings together
   static Future<ui.Image> _bankCardWorker(details) async {
+    final gpayImg = await loadUiImage('assets/images/gpay.png');
+    final imge = await loadUiImage('assets/images/axis.png');
     final ui.PictureRecorder recorder = ui.PictureRecorder();
     final Canvas canvas = Canvas(
         recorder, Rect.fromPoints(Offset(0.0, 0.0), Offset(30.0, 600.0)));
     final stroke = Paint();
-    try {
-      var imge = await loadUiImage('assets/images/axis.png');
 
-      canvas.drawImage(imge, Offset(0, 0), stroke);
-    } catch (e) {
-      print(e);
-    }
-    var pH = printParagraph(canvas:canvas, offset:Offset(75, 150,),fromMap: true,map:details);
+    canvas.drawImage(imge, Offset(0, 0), stroke);
+    var pH = printParagraph(
+        canvas: canvas,
+        offset: Offset(
+          75,
+          150,
+        ),
+        fromMap: true,
+        widthConstraint: 830,
+        map: details);
+
     stroke.color = Colors.white;
     stroke.style = PaintingStyle.fill;
-
-    canvas.drawRRect(
-        RRect.fromRectAndRadius(
-            Offset(75, 150 + pH + 20) & Size(700, 100), Radius.circular(20)),
-        stroke);
+    printGpay(
+        canvas: canvas,
+        no: details['Gpay'],
+        offset: Offset(75, 150 + pH + 20),
+        img: gpayImg);
+    // canvas.drawRRect(
+    //     RRect.fromRectAndRadius(
+    //         Offset(75, 150 + pH + 20) & Size(700, 100), Radius.circular(20)),
+    //     stroke);
 
     final picture = recorder.endRecording();
     final img = await picture.toImage(930, 600);
@@ -76,10 +150,11 @@ class ImgFromTempelate {
 
 // The core function which genrates the compressed PNG Image
   static Future<ByteData> pngBytes(ui.Image img) async {
-  
-    final pngBytes = await img.toByteData(format: ui.ImageByteFormat.png);
-    final buffer = pngBytes.buffer;
+    var pngBytes;
+    var buffer;
     try {
+      pngBytes = await img.toByteData(format: ui.ImageByteFormat.png);
+      buffer = pngBytes.buffer;
       if (await Permission.storage.request().isGranted) {
         var directory = await ExtStorage.getExternalStorageDirectory();
         await File(directory +
@@ -89,16 +164,22 @@ class ImgFromTempelate {
             .writeAsBytes(buffer.asUint8List());
       }
     } catch (err) {
+      print('pngBytes : An error occured');
       print(err);
     }
 
     return pngBytes;
-    
   }
 
   // generate BankCard
-  static Future<ByteData> generateBankCard(Map<String,String> details) async{
-    final img = await _bankCardWorker(details);
+  static Future<ByteData> generateBankCard(Map<String, String> details) async {
+    var img;
+    try {
+      img = await _bankCardWorker(details);
+    } catch (e) {
+      print('generateBankCard : Error Occured');
+      throw Future.error(e);
+    }
     return pngBytes(img);
   }
 }
